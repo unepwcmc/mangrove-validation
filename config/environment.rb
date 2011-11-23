ecd9d857a6771fd3867e492ba0061e21a70bf5b5
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-11-23 13:54:15 +0000

    freeze gem version

--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "authlogic", :version => '2.1.4'
-  config.gem "faker"
-  config.gem "hashie"
+  config.gem "faker", :version => "0.9.5"
+  config.gem "hashie", :version => "0.2.0"
+  config.gem "pg", :version => "0.11.0"
   #config.gem "ngmoco-cache-money", :lib => "cache_money"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
