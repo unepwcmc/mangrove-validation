7c5dc75c46cbe6f1266262714fb1011a3742b352
Simao Belchior de Castro <simaobelchior@gmail.com>
2011-11-23 16:25:20 +0000

    fixing layout of landing page.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,9 @@ class MainController < ApplicationController
   before_filter :require_user, :only => [:main] 
 
   def index
+    @user_session = UserSession.new
+    if current_user
+      redirect_to games_path
+    end
   end
 end
