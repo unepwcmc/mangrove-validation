7cd3d47416958716f30a2c34d819c8a6e6696a87
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-02-24 16:08:37 +0000

    delete intersection when user adds a polygon

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -20,14 +20,14 @@ class Layer < ActiveRecord::Base
       when 'add'
         sql = <<-SQL
           INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, name, status)
-          VALUES (ST_GeomFromText('MULTIPOLYGON(((#{polygon})))', 4326), #{NAMES.index(name)}, 1)
+          VALUES (ST_GeomFromText('MULTIPOLYGON(((#{polygon})))', 4326), #{NAMES.index(name)}, 1);
+          UPDATE #{APP_CONFIG['cartodb_table']} SET the_geom=ST_Multi(ST_Union(ST_Difference(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)), ST_GeomFromEWKT('SRID=4326;POLYGON EMPTY'))) WHERE ST_Intersects(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)) AND status = 0 AND name = #{NAMES.index(name)};
         SQL
       when 'delete'
         sql = <<-SQL
-          UPDATE #{APP_CONFIG['cartodb_table']} SET the_geom=ST_Multi(ST_Union(ST_Difference(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)), ST_GeomFromEWKT('SRID=4326;POLYGON EMPTY'))) WHERE ST_Intersects(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326))
+          UPDATE #{APP_CONFIG['cartodb_table']} SET the_geom=ST_Multi(ST_Union(ST_Difference(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)), ST_GeomFromEWKT('SRID=4326;POLYGON EMPTY'))) WHERE ST_Intersects(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)) AND name = #{NAMES.index(name)}
         SQL
     end
-
     CartoDB::Connection.query sql
   rescue CartoDB::Client::Error
     errors.add :base, 'There was an error trying to render the layers.'
