acc583bbe3224b6765f8810d93632718569a9fbc
Simao Belchior de Castro <simaobelchior@gmail.com>
2012-02-24 16:49:18 +0000

    updated queries to avoid overlapping polygons in community version of the layers

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -15,12 +15,14 @@ class Layer < ActiveRecord::Base
       when 'validate'
         sql = <<-SQL
           INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, name, status)
-          (SELECT ST_Multi(ST_Intersection(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326))), #{NAMES.index(name)}, 1 FROM #{APP_CONFIG['cartodb_table']} WHERE ST_Intersects(the_geom, ST_GeomFromText('SRID=4326;POLYGON((#{polygon}))', 4326)))
+          (SELECT ST_Multi(ST_Intersection(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326))), #{NAMES.index(name)}, 1 FROM #{APP_CONFIG['cartodb_table']} WHERE ST_Intersects(the_geom, ST_GeomFromText('SRID=4326;POLYGON((#{polygon}))', 4326)) AND status = 0 AND name = #{NAMES.index(name)})
         SQL
       when 'add'
         sql = <<-SQL
           INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, name, status)
-          VALUES (ST_GeomFromText('MULTIPOLYGON(((#{polygon})))', 4326), #{NAMES.index(name)}, 1);
+            (SELECT ST_Multi(ST_Union(ST_Difference(ST_GeomFromText('POLYGON((#{polygon}))', 4326), the_geom), ST_GeomFromEWKT('SRID=4326;POLYGON EMPTY'))), #{NAMES.index(name)}, 1
+              FROM #{APP_CONFIG['cartodb_table']}
+              WHERE ST_Intersects(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)) AND status = 1 AND name = #{NAMES.index(name)});
           UPDATE #{APP_CONFIG['cartodb_table']} SET the_geom=ST_Multi(ST_Union(ST_Difference(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)), ST_GeomFromEWKT('SRID=4326;POLYGON EMPTY'))) WHERE ST_Intersects(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326)) AND status = 0 AND name = #{NAMES.index(name)};
         SQL
       when 'delete'
@@ -31,5 +33,6 @@ class Layer < ActiveRecord::Base
     CartoDB::Connection.query sql
   rescue CartoDB::Client::Error
     errors.add :base, 'There was an error trying to render the layers.'
+    puts "There was an error trying to execute the following query:\n#{sql}"
   end
 end
