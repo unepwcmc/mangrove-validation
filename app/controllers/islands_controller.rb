7a42214250a060595d28f2efe579f957eee6d828
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-05-25 10:51:22 +0100

    Islands index action

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -2,7 +2,7 @@ class IslandsController < ApplicationController
   respond_to :json
 
   def index
-    @islands = Island.all
+    @islands = Island.filter(params)
     respond_with @islands
   end
 end
--- app/models/island.rb
+++ app/models/island.rb
@@ -1,2 +1,13 @@
 class Island < ActiveRecord::Base
+  def self.filter(params)
+    if (params.symbolize_keys.keys & [:id, :query]).empty?
+      all
+    else
+      clause = {}
+      clause[:id] = params[:id] if params[:id]
+      clause[:query] = params[:query] if params[:query]
+
+      where(clause)
+    end
+  end
 end
