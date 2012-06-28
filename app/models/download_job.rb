99918c2e70fa9bd57291a214b2f0de85d2fac5ff
Adam Mulligan <adammulligan@gmail.com>
2012-06-28 16:19:46 +0100

    ogr2ogr now runs on small batches of geojson data and merged together, which should solve the massive memory requirements

--- app/models/download_job.rb
+++ app/models/download_job.rb
@@ -26,36 +26,45 @@ class DownloadJob
       # Result count
       count = JSON.parse(res.body)["rows"].first["count"]
 
-      filename = "#{Rails.root}/tmp/download#{SecureRandom.hex(5)}.json",
-      File.open(filename, 'w+') do |file|
-        0.upto (count/APP_CONFIG['admin_user_edits_limit']).round do |offset|
+      hash = SecureRandom.hex(5)
+      ogr2ogr_dir = self.ogr2ogr_directory(:user_geo_edit, user_geo_edit_download.id)
+      zip_dir = self.zip_path(:user_geo_edit, user_geo_edit_download.id)
+
+      0.upto (count/APP_CONFIG['admin_user_edits_limit']).round do |offset|
+        File.open(self.temp_file_name(hash, offset), 'w+') do |file|
           query = "SELECT * FROM #{APP_CONFIG['cartodb_table']}"
           query << " WHERE #{where_clause}" if !user_geo_edit_download.island_ids.empty?
           query << " LIMIT #{APP_CONFIG['admin_user_edits_limit']} OFFSET #{offset * APP_CONFIG['admin_user_edits_limit']}"
           uri = URI.parse(URI.escape("http://carbon-tool.cartodb.com/api/v1/sql?q=#{query}&format=geojson"))
           res = Net::HTTP.get_response(uri)
           file << res.body.force_encoding('UTF-8')
-          puts "Processing #{offset}/#{count}"
+
+          puts "Processing #{offset+1}/#{count / APP_CONFIG['admin_user_edits_limit']}"
+          puts "Saving to file #{file.path}"
         end
+
+        # Debug
+        # system "cp #{filename} ~/Desktop"
+
+        puts "ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' #{ogr2ogr_dir}/#{offset} #{self.temp_file_name(hash, offset)}"
+        puts `ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' #{ogr2ogr_dir}/#{offset} #{self.temp_file_name(hash, offset)}`
+        if $? != 0 then raise end
+
+        ogr_command = "ogr2ogr "
+        ogr_command << "-update -append " if offset>0
+        ogr_command << "#{ogr2ogr_dir}/all.shp #{ogr2ogr_dir}/#{offset}/OGRGeoJSON.shp"
+        puts ogr_command
+        system ogr_command
       end
 
-      ogr2ogr_dir = self.ogr2ogr_directory(:user_geo_edit, user_geo_edit_download.id)
-
-      # Debug
-      # system "cp #{filename} ~/Desktop"
-
-      puts "ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' #{ogr2ogr_dir} #{filename}"
-      puts `ogr2ogr -overwrite -skipfailures -f 'ESRI Shapefile' #{ogr2ogr_dir} #{filename}`
-      if $? != 0 then raise end
-
-      puts "zip -j #{self.zip_path(:user_geo_edit, user_geo_edit_download.id)} #{ogr2ogr_dir}/*"
-      puts `zip -j #{self.zip_path(:user_geo_edit, user_geo_edit_download.id)} #{ogr2ogr_dir}/*`
+      puts "zip -j #{zip_dir} #{ogr2ogr_dir}/all.*"
+      puts `zip -j #{zip_dir} #{ogr2ogr_dir}/all.*`
       if $? != 0 then raise end
 
       # Move the file to a download directory (in /public)
       # Replace this and #download_directory to use something like S3
-      puts "mv #{self.zip_path(:user_geo_edit, user_geo_edit_download.id)} #{self.download_directory(:user_geo_edit)}"
-      puts `mv #{self.zip_path(:user_geo_edit, user_geo_edit_download.id)} #{self.download_directory(:user_geo_edit)}`
+      puts "mv #{zip_dir} #{self.download_directory(:user_geo_edit)}"
+      puts `mv #{zip_dir} #{self.download_directory(:user_geo_edit)}`
       if $? != 0 then raise end
 
       puts "Successfully generated a download for #{user_geo_edit_download.id}"
@@ -64,6 +73,10 @@ class DownloadJob
       puts msg
       user_geo_edit_download.update_attributes(:status => :failed)
     end
+  end
+
+  def self.temp_file_name(hash, num)
+    "#{Rails.root}/tmp/download#{hash}-#{num}.json"
   end
 
   def self.download_directory(type)
