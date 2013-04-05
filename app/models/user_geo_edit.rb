b9db194846bcbce194bfebfbc5e8e4eac07410cb
Adam Mulligan <adammulligan@gmail.com>
2013-04-05 14:48:08 +0100

    Renamed polygon_id inline with cartodb changes

--- app/models/user_geo_edit.rb
+++ app/models/user_geo_edit.rb
@@ -17,7 +17,7 @@ class UserGeoEdit < ActiveRecord::Base
       when 'validate'
         # Insert validated area polygon
         sql = <<-SQL
-          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, polygon_id, iso_3, name, name_local, created_at, updated_at)
+          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, id_gid, iso_3, name, name_local, created_at, updated_at)
             (SELECT ST_Multi(ST_Intersection(the_geom, #{geom_sql})), #{island_id}, 'validated', '#{action}', '#{user.email}', '#{user.institution}', '#{user.name}', '#{time_in_milliseconds}', '#{h(island.iso_3)}', '#{h(island.name)}', '#{h(island.name_local)}', '#{island.created_at}', '#{island.updated_at}'
               FROM #{APP_CONFIG['cartodb_table']}
               WHERE ST_Intersects(the_geom, #{geom_sql}) AND status = 'original' AND island_id = #{island_id});
@@ -31,7 +31,7 @@ class UserGeoEdit < ActiveRecord::Base
       when 'reallocate'
         # Insert copy polygon area from reallocated_from_island
         sql = <<-SQL
-          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, name, polygon_id)
+          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, name, id_gid)
             (SELECT ST_Multi(ST_Intersection(the_geom, #{geom_sql})), #{island_id}, 'validated', '#{action}', '#{user.email}', '#{user.institution}', '#{user.name}', '#{time_in_milliseconds}'
               FROM #{APP_CONFIG['cartodb_table']}
               WHERE ST_Intersects(the_geom, #{geom_sql}) AND island_id = #{reallocated_from_island_id} AND status IS NOT NULL);
@@ -52,7 +52,7 @@ class UserGeoEdit < ActiveRecord::Base
 
         # Add the user geometry, minus existing validations (using ST_Difference if any polys intersect)
         sql = <<-SQL
-          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, polygon_id, iso_3, name, name_local, created_at, updated_at) 
+          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, id_gid, iso_3, name, name_local, created_at, updated_at) 
             SELECT
               ST_Multi(CASE WHEN existing_validations.the_geom IS NOT NULL THEN 
                 ST_Difference(
@@ -76,7 +76,7 @@ class UserGeoEdit < ActiveRecord::Base
       when 'delete'
         # Insert deleted area
         sql = <<-SQL
-          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, polygon_id, iso_3, name, name_local, created_at, updated_at)
+          INSERT INTO #{APP_CONFIG['cartodb_table']} (the_geom, island_id, status, action, email, institution, user_name, id_gid, iso_3, name, name_local, created_at, updated_at)
             (SELECT ST_Multi(ST_Intersection(the_geom, ST_GeomFromText('POLYGON((#{polygon}))', 4326))), #{island_id}, NULL, '#{action}', '#{user.email}', '#{user.institution}', '#{user.name}', '#{time_in_milliseconds}', '#{h(island.iso_3)}', '#{h(island.name)}', '#{h(island.name_local)}', '#{island.created_at}', '#{island.updated_at}'
               FROM #{APP_CONFIG['cartodb_table']}
               WHERE ST_Intersects(the_geom, ST_GeomFromText('SRID=4326;POLYGON((#{polygon}))', 4326)) AND status IS NOT NULL AND island_id = #{island_id});
