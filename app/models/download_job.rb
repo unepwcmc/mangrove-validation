a456334fcdc978673ac06ff7580c8e0d16d48bb7
Adam Mulligan <adammulligan@gmail.com>
2013-04-08 11:16:48 +0100

    Updated download job to reduce memory footprint when generating zip files (by creating the zip rather than appending)

--- app/models/download_job.rb
+++ app/models/download_job.rb
@@ -6,33 +6,38 @@ class DownloadJob
   end
 
   def initialize(id)
-    download = UserGeoEditDownload.find(id)
-    puts "Generating download for ID #{download.id} (Name: #{download.name}; Islands: #{download.island_ids}) at #{Time.now}"
+    @download = UserGeoEditDownload.find(id)
+    puts "Generating download for ID #{@download.id} (Name: #{@download.name}; Islands: #{@download.island_ids}) at #{Time.now}"
 
     generate_download
 
-    puts "Successfully generated download for ID #{download.id}"
-    download.update_attributes(:file_id => cache_id)
-    download.update_attributes(:status => :finished)
+    puts "Successfully generated download for ID #{@download.id}"
+    @download.update_attributes(:file_id => cache_id)
+    @download.update_attributes(:status => :finished)
 
-    puts "Sending notification mail to #{download.user.email}"
-    DownloadNotifier.download_email(download).deliver
+    begin
+      puts "Sending notification mail to #{@download.user.email}"
+      DownloadNotifier.download_email(@download).deliver
+    rescue Exception => msg
+      DownloadJob.print_error(msg)
+    end
   rescue Exception => msg
     DownloadJob.print_error(msg)
     cleanup
-    download.update_attributes(:status => :failed)
+    @download.update_attributes(:status => :failed)
   end
 
   private
 
   def cleanup
-    FileUtils.rm_rf(cache_file)
+    FileUtils.rm_rf(download_file)
   end
 
   def generate_download
     if !File.exists?(cache_file)
       download_all_islands
       add_readme_to_download
+      cleanup
     end
   end
 
@@ -48,6 +53,12 @@ class DownloadJob
     Digest::SHA1.hexdigest(edits_ids_json)
   end
 
+  def download_directory
+    path = "#{Rails.root}/public/exports"
+    FileUtils.mkdir_p path unless File.exists?(path)
+    path
+  end
+
   def cache_directory
     path = "#{download_directory}/cache"
     FileUtils.mkdir_p path unless File.exists?(path)
@@ -58,6 +69,10 @@ class DownloadJob
     return "#{cache_directory}/#{cache_id}.zip"
   end
 
+  def download_file
+    return "#{cache_directory}/#{@download.id}.zip"
+  end
+
   def download_url
     query   = URI.encode("SELECT * FROM #{APP_CONFIG['cartodb_view']}")
     api_key = CARTODB_CONFIG['api_key']
@@ -66,22 +81,18 @@ class DownloadJob
 
   def download_all_islands
     require 'open-uri'
-    open(cache_file, "wb") do |fo|
+    open(download_file, "wb") do |fo|
       fo.print open(download_url).read
     end
   end
 
   def add_readme_to_download
     Dir.chdir(download_directory) do
-      `zip -r #{cache_file} README.txt`
-      `zip -r #{cache_file} General_Data_License_UNEP-WCMC.pdf`
+      island_file = "#{cache_directory}/islands.zip"
+      `ln -s #{download_file} #{island_file}`
+      `zip -j #{cache_file} #{island_file} README.txt General_Data_License_UNEP-WCMC.pdf`
+      `rm #{island_file}`
     end
-  end
-
-  def download_directory
-    path = "#{Rails.root}/public/exports"
-    FileUtils.mkdir_p path unless File.exists?(path)
-    path
   end
 
   def self.print_error(exception)
