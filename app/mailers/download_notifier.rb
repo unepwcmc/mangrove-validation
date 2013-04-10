7c6fbad3768c9c33f14ddba0164d7208527a5cb5
Adam Mulligan <adammulligan@gmail.com>
2013-04-10 12:51:04 +0100

    Added licensing information attachments to download notification emails

--- app/mailers/download_notifier.rb
+++ app/mailers/download_notifier.rb
@@ -6,6 +6,8 @@ class DownloadNotifier < ActionMailer::Base
   def download_email(download)
     @user = download.user
     @download = download
+    attachments['General_Data_License_UNEP-WCMC.pdf'] = File.read("#{Rails.root}/public/exports/General_Data_License_UNEP-WCMC.pdf")
+    attachments['MetadataInformation.zip'] = File.read("#{Rails.root}/public/exports/MetadataInformation.zip")
 
     mail(:to => @user.email,
          :subject => "Your Global Islands Database download is complete!",
