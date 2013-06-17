b8d91a316f630ad9e1657ae0a22099c5bf19b358
Adam Mulligan <adammulligan@gmail.com>
2013-06-17 08:49:45 +0100

    Search should only search from the beginning of the island name, rather than anywhere inside

--- app/models/island.rb
+++ app/models/island.rb
@@ -9,7 +9,7 @@ class Island < ActiveRecord::Base
     # This could be done with query.to_i.to_a?(Numeric) which seems more robust
     # but this would result in things like "3a" -> "3" which is not ideal.
     if (params[:query] =~ /^[0-9]+$/).nil?
-      result = result.where("name ILIKE '%#{params[:query]}%'")
+      result = result.where("name ILIKE '#{params[:query]}%'")
     else
       result = result.where(id: params[:query].to_i)
     end
